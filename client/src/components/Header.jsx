e167a81473c963863533f0b6a77739181673f923
cdhakad15 <chetandhakad1505@gmail.com>
2024-01-10 02:53:52 +0530

    complete signup page functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
       </Link>
      
 
-        <form className='bg-slate-300 p-3 rounded-lg flex items-center'>
+        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
           <input type="text" placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64'  />
           <FaSearch className='text-slate-600'/>
         </form>
