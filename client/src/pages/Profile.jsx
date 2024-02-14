5fcfff4451268c8ead5efd871de8d4232ce201ff
cdhakad15 <chetandhakad1505@gmail.com>
2024-02-15 02:08:04 +0530

    complete image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,71 +1,89 @@
 import React from 'react'
 import { Link , useNavigate} from 'react-router-dom'
-import { useState } from 'react';
-import OAuth from '../components/OAuth';
+import { useState ,useEffect} from 'react';
+import { useRef } from 'react';
 import {useSelector} from "react-redux";
-
+import {getStorage, uploadBytesResumable, ref, getDownloadURL } from "firebase/storage"
+import {app} from "../firebase"
 
 const Profile = () => {
+  const fileRef = useRef(null)
 
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(null);
+  // const [loading, setLoading] = useState(false);
   const {currentUser} = useSelector(state=>state.user)
+  const [file,saveFile] = useState(undefined);
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+ 
 
-  const navigate= useNavigate();
-  const handleChange= (e)=>{
-       setFormData(
-         {
-          ...formData,
-          [e.target.id]:e.target.value,
-        });
+//  firebase storage
+// allow read;
+// allow write :if
+// request.resource.size < 2*1024*1024 &&
+// request.resource.contentType.matches("image/.*")
+
+
+ 
+     
+     
+     useEffect(() => {
+      if(file){
+        handleFileUpload(file);
+      }
+     }, [file]);
+
+     const handleFileUpload =(file)=>{
+      const storage = getStorage(app);
+      const fileName = new Date().getTime()+ file.name;
+      const storageRef = ref(storage,fileName);
+      const uploadTask = uploadBytesResumable(storageRef,file);
+
+    uploadTask.on(
+      "state_changed",
+    (snapshot)=>{
+      const progress = (snapshot.bytesTransferred/snapshot.totalBytes)*100;
+      setFilePerc(Math.round(progress));
+    },
+
+    (error)=>{
+      setFileUploadError(true);
+    },
+    ()=>{
+      getDownloadURL(uploadTask.snapshot.ref).then
+      ((downloadURL)=>{
+        setFormData({...formData, avatar:downloadURL}) ;  
+      });
+    })
+    
   };
-
-  const handleSubmit = async (e)=>{   
-    e.preventDefault();
-
-    try{
-      setLoading(true);
-     const res = await fetch("api/auth/signup",
-     {
-      method:"POST",
-      "headers":{
-        "Content-Type":"application/json",
-      },
-      body:JSON.stringify(formData),
-     });
-
-     const data = await res.json();
-     if(data.success === false){
-      setError(data.message);
-      setLoading(false);
-      return;
-     }
-
-     setLoading(false);
-     navigate("/sign-in");
-    
-    }
-    catch(error){
-      setLoading(false);
-      setError(error.message);
-    }
-
-    
-     };
-   
+     
 
    
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Profile</h1>
-      <form onSubmit={handleSubmit} className='flex flex-col gap-4 ' >
-      <img src={currentUser.avatar} alt="" className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'/>
-        <input type="name" placeholder='Username' className='border p-3 rounded-lg ' id='username'  onChange={handleChange}  />
-        <input type="email" placeholder='Email' className='border p-3 rounded-lg ' id='email'  onChange={handleChange}/>
-        <input type="password" placeholder='Password' className='border p-3 rounded-lg ' id='password' onChange={handleChange} />
-         <button  disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
-         {loading ? "Loading..." : "Update"}
+      <form className='flex flex-col gap-4 ' >
+       <input onChange={(e)=>saveFile(e.target.files[0])} type="file" ref={fileRef} hidden accept='image/*'/>
+        <img  onClick={()=>fileRef.current.click()} src={formData.avatar || currentUser.avatar} alt="" className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'/>
+        <p className='text-sm self-center'>
+        {fileUploadError?
+        (<span className='text-red-700'> Error Image upload(image must be less than 2 mb)</span>):
+        filePerc>0 && filePerc<100 ?(
+          <span className='text-slate-700'>{`Uploading ${filePerc}%`}</span>
+        ):filePerc===100 ?(
+          <span className='text-green-700 '>Image successfully uploaded!</span>
+        ):(
+           ""
+        )}
+
+        </p>
+        <input type="name" placeholder='Username' className='border p-3 rounded-lg ' id='username'    />
+        <input type="email" placeholder='Email' className='border p-3 rounded-lg ' id='email'  />
+        <input type="password" placeholder='Password' className='border p-3 rounded-lg ' id='password'  />
+         <button   className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+         Update
          </button> 
 
          
@@ -77,9 +95,10 @@ const Profile = () => {
          <span className='text-red-700 cursor-pointer'>Sign out</span>
         </Link>
        </div>
-    {error && <p className='text-red-500 mt-5'>{error}</p>}
+    {/* {error && <p className='text-red-500 mt-5'>{error}</p>} */}
     </div>
   )
 }
 
 export default Profile
+
