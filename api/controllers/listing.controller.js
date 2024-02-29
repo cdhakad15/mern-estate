fc5118406e39e7749abbd6506f9e7bf1afcaab27
cdhakad15 <chetandhakad1505@gmail.com>
2024-02-29 16:35:01 +0530

    create updare listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js"
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async(req,res,next)=>{
     try {
@@ -31,6 +32,31 @@ export const deleteListing = async (req, res, next) => {
 };
 
 
+
+export const updateListing = async(req,res,next)=>{
+  const listing = await Listing.findById(req.params.id);
+  if(!listing){
+    return next(errorHandler(404, "Listing not found"))
+  }
+  
+  if(req.params.id !== listing.userRef){
+    return next(errorHandler(401, "You can only update own listings"));
+  }
+
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {new:true}
+   
+     );
+     res.status(200).json(updatedListing);
+   
+  } catch (error) {
+      next(error);
+  }
+  
+} 
 
 
 
