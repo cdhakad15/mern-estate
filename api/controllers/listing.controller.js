8ad2164beba7ae6e9707aa359473016878d278d8
cdhakad15 <chetandhakad1505@gmail.com>
2024-02-29 19:21:06 +0530

    complete update listing functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -39,7 +39,7 @@ export const updateListing = async(req,res,next)=>{
     return next(errorHandler(404, "Listing not found"))
   }
   
-  if(req.params.id !== listing.userRef){
+  if(req.user.id !== listing.userRef){
     return next(errorHandler(401, "You can only update own listings"));
   }
 
@@ -55,8 +55,21 @@ export const updateListing = async(req,res,next)=>{
   } catch (error) {
       next(error);
   }
-  
-} 
+
+}
+
+
+export const getListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return next(errorHandler(404, 'Listing not found!'));
+    }
+    res.status(200).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
 
 
 
